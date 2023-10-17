4eadc6027b305bbac2660e9e6815a05164d56a3d
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-10-18 00:22:23 +0330

    prettier extension added to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <h1 className="text-center text-3xl font-bold underline ">
+        Hello world!
+      </h1>
     </>
   );
 }
